88c579f3b158a7930eaf7d0bb0655c5bb3942f81
D'Lorean Bell <dloreanbell@gmail.com>
2018-02-08 04:01:28 -0500

    main menu changes

--- javascript.js
+++ javascript.js
@@ -1,14 +1,32 @@
 $(function(){
 
+
+
 // JavaScript-y things here
 
 // counts from the menu selection
+
+var itemQuantity;
+$(":input").bind('keyup mouseup', function () {
+    itemQuantity = $(this).val();
+    // alert(itemQuantity);
+    console.log(itemQuantity*5);
+
+});
+
+
+
+
+
+
 // these need ids for assignment later
 // assign these values on click of checkout button
 
 itemCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // intialize prices variables
+
+
 // set prices in JavaScript
 itemPrices = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
 
@@ -17,10 +35,12 @@ var salesTax = 0.06;
 var totalOwed;
 
 function getSubtotal() {
-  for (i = 0; i < 12; i++) {
+  // for (i = 0; i < 12; i++) {
 
-  }
-  return subtotal
+  // }
+
+  (5 * 5);
+  
 }
 
 function applyTax(preTaxTotal) {
@@ -28,10 +48,10 @@ function applyTax(preTaxTotal) {
 }
 
 function getTotal() {
-  return applyTax(getSubtotal());
+  // return applyTax(getSubtotal());
 }
 
-$("#menuButton").click(function() {
+$("#checkoutButton").click(function() {
   var total = getSubtotal();
   totalOwed = applyTax(total);
 });
