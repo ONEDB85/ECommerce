c73350491bcbba44155e35ca78b17941231b3cb1
D'Lorean Bell <dloreanbell@gmail.com>
2018-02-10 20:53:15 -0500

    DB changes to menu and welcome

--- javascript.js
+++ javascript.js
@@ -8,90 +8,80 @@ $(function() {
         var productNum = parseInt(inputEl.attr("data-product-num"));
         var count = parseInt(inputEl.val());
         menuItems[productNum].count = count;
+        var added = $(this).text("added");
+       
     });
 
-// array of menu objects
+    // array of menu objects
     var menuItems = [{
             item: "Pint Glass",
             cat: "Housewares",
-            desc: "clear glass",
             price: 1,
             count: 0
         },
         {
             item: "Ceramic Mug",
             cat: "Housewares",
-            desc: "",
             price: 5,
             count: 0
         },
         {
             item: "Travel Mug",
             cat: "Housewares",
-            desc: "",
             price: 10,
             count: 0
         },
         {
             item: "T-Shirt",
             cat: "Apparel",
-            desc: "",
             price: 4,
             count: 0
         },
         {
             item: "Sweatshirt",
             cat: "Apparel",
-            desc: "",
             price: 13,
             count: 0
         },
         {
             item: "Tank Top",
             cat: "Apparel",
-            desc: "",
             price: 7,
             count: 0
         },
         {
             item: "Knit Hat",
             cat: "Apparel",
-            desc: "",
             price: 20,
             count: 0
         },
         {
             item: "Baseball Cap",
             cat: "Apparel",
-            desc: "",
             price: 35,
             count: 0
         },
         {
             item: "Poster",
             cat: "Furnishings",
-            desc: "",
             price: 8,
             count: 0
         },
         {
             item: "Headphones",
             cat: "Electronics",
-            desc: "",
             price: 25,
             count: 0
         },
         {
             item: "MP3 Player",
             cat: "Electronics",
-            desc: "",
             price: 67,
             count: 0
         },
         {
             item: "Tablet",
             cat: "Electronics",
-            desc: "",
             price: 250,
             count: 0
         },
@@ -103,7 +93,7 @@ $(function() {
     var taxTotal = 0;
     var netTotal = 0;
 
-  // goes to cart page
+    // goes to cart page
     $("#checkout").click(function() {
         cart = menuItems.filter(function(item) {
             return item.count > 0;
@@ -114,10 +104,10 @@ $(function() {
         taxTotal = subtotal * tax;
         taxTotal = parseFloat(taxTotal.toFixed(2));
         netTotal = subtotal + taxTotal;
-        cart.forEach(function(item){
-          $("#productQuantity").append(
-            "<li>" + item.count + " - " + item.item + "</li>"
-          );
+        cart.forEach(function(item) {
+            $("#productQuantity").append(
+                "<li>" + item.count + " - " + item.item + "</li>"
+            );
         });
         $("#cartSubTotal").text(subtotal.toFixed(2));
         $("#cartTax").text(taxTotal.toFixed(2));
@@ -127,79 +117,51 @@ $(function() {
         buildReceipt();
     });
 
-// goes to cash change screen
+    // goes to cash change screen
     $("#cashbutton").click(function() {
-      event.preventDefault();
-      $("#cashTotal").text(netTotal);
-      $("#cart").toggleClass("hidden");
-      $("#cash").toggleClass("hidden");
+        event.preventDefault();
+        $("#cashTotal").text(netTotal);
+        $("#cart").toggleClass("hidden");
+        $("#cash").toggleClass("hidden");
     });
 
-// goes to credit card screen
+    // goes to credit card screen
     $("#creditbutton").click(function() {
-      $("#cart").toggleClass("hidden");
-      $("#credit").toggleClass("hidden");
+        $("#cart").toggleClass("hidden");
+        $("#credit").toggleClass("hidden");
     });
 
-// makes change
+    // makes change
     var tendered = 0;
     $("#cashPay").click(function() {
-      tendered = parseFloat($("#cashTendered").val());
-      var change = tendered - netTotal;
-      change = parseFloat(change.toFixed(2));
-      $("#change").text(change.toFixed(2));
-      $("#changeOutput").toggleClass("hidden");
-      $("#toReceipt").toggleClass("hidden");
+        tendered = parseFloat($("#cashTendered").val());
+        var change = tendered - netTotal;
+        change = parseFloat(change.toFixed(2));
+        $("#change").text(change.toFixed(2));
+        $("#changeOutput").toggleClass("hidden");
+        $("#toReceipt").toggleClass("hidden");
     })
 
     // print receipt button on cash page
     $("#toReceipt").click(function() {
-      $("#cash").toggleClass("hidden");
-      $("#receipt").toggleClass("hidden");
+        $("#cash").toggleClass("hidden");
+        $("#receipt").toggleClass("hidden");
     });
 
     // cc pay does nothing except show receipt
     $("#ccPay").click(function() {
-      $("#credit").toggleClass("hidden");
-      $("#receipt").toggleClass("hidden");
+        $("#credit").toggleClass("hidden");
+        $("#receipt").toggleClass("hidden");
     })
 
-// $( "#outer" ).mouseleave(function() {
-//   $( "#log" ).append( "<div>Handler for .mouseleave() called.</div>" );
-// });
-
-// $("#images").mouseenter(function(){
-//     $( "#images" ).mouseenter(function() {
-//   $(this).append( '<div>Handler for .mouseenter() called.</div>' );
-// });
-    
-//     $( "#images" ).mouseleave(function() {
-//   $(this).addclass("hoverDescrip");
-// });
-//     if(!block) {
-//         block = true;
-//         $(this).append( '<div>Handler for .mouseenter() called.</div>' ).slideDown(400, function(){
-//             block = false;
-//         });
-//     }
-// });
-// $("#images").mouseleave(function(){
-//     if(!block) {
-//         block = true;
-//         $(this).append( '<div>Handler for .mouseenter() called.</div>' ).slideUp(400, function(){
-//             block = false;
-//         });
-//     }
-// });
-
     function buildReceipt() {
-      cart.forEach(function(product) {
-        for (i = 0; i < product.count; i++) {
-          $("#receipt").append("<p> " + product.item + " - $" + product.price + "</p>");
-        }
-      });
-      $("#receipt").append("<p> subtotal: $" + subtotal + "</p>");
-      $("#receipt").append("<p> tax: $" + taxTotal + "</p>");
-      $("#receipt").append("<p> total: $" + netTotal + "</p>");
+        cart.forEach(function(product) {
+            for (i = 0; i < product.count; i++) {
+                $("#receipt").append("<p> " + product.item + " - $" + product.price + "</p>");
+            }
+        });
+        $("#receipt").append("<p> subtotal: $" + subtotal + "</p>");
+        $("#receipt").append("<p> tax: $" + taxTotal + "</p>");
+        $("#receipt").append("<p> total: $" + netTotal + "</p>");
     }
 });
